53d5f45903c113b3662a35c7d9b0833c4410a8e6
Kaesek <fcomirandaguajardo@gmail.com>
2023-04-23 14:29:17 -0400

    main

--- donarfunciones.js
+++ donarfunciones.js
@@ -1,10 +1,19 @@
-var donar = 0;
-donar = document.getElementById("donacion");
 function hacerDonacion(){
-    console.log(donar)
-  if(donar < 0){
-    alert("Tu donacion fue recibida con exito!");
+  var valorInput = document.getElementById("donacion").value;
+  console.log(valorInput)
+
+  if(valorInput > 0){
+    alert("Tu donacion fue exitosa!!")
   }else{
-    alert("No puedes hacer donaciones por este monto");
+    alert("Tu donacion no puede ser menor a 1 peso")
   }
 };
+
+function mostrarDonacion(){
+  var valorInput = document.getElementById("miDiv");
+  div.innerHTML = mostrarDonacion();
+
+}
+
+
+
