7e91ac9f9512cfe5589d4cd0feda70e6f04e1c37
IsaacAC123 <isaac.cari.a.c@gmail.com>
2023-04-23 15:11:17 -0400

    FinalAhorasi

--- donarfunciones.js
+++ donarfunciones.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+HEAD;
 function hacerDonacion() {
   var valorInput = document.getElementById("donacion").value;
   console.log(valorInput);
@@ -14,24 +14,19 @@ function mostrarDonacion() {
   var valorInput = document.getElementById("miDiv");
   div.innerHTML = mostrarDonacion();
 }
-=======
-function hacerDonacion(){
+
+function hacerDonacion() {
   var valorInput = document.getElementById("donacion").value;
-  console.log(valorInput)
+  console.log(valorInput);
 
-  if(valorInput > 0){
-    alert("Tu donacion fue exitosa!!")
-  }else{
-    alert("Tu donacion no puede ser menor a 1 peso")
+  if (valorInput > 0) {
+    alert("Tu donacion fue exitosa!!");
+  } else {
+    alert("Tu donacion no puede ser menor a 1 peso");
   }
-};
-
-function mostrarDonacion(){
-  var valorInput = document.getElementById("miDiv");
-  div.innerHTML = mostrarDonacion();
-
 }
 
-
-
->>>>>>> 53d5f45903c113b3662a35c7d9b0833c4410a8e6
+function mostrarDonacion() {
+  var valorInput = document.getElementById("miDiv");
+  div.innerHTML = mostrarDonacion();
+}
