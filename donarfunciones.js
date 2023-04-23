bb8f8efc0579a44f8269a82877441131b91eee21
IsaacAC123 <isaac.cari.a.c@gmail.com>
2023-04-23 15:00:41 -0400

    MisPerrisFinal

---
+++ donarfunciones.js
@@ -0,0 +1,15 @@
+function hacerDonacion() {
+  var valorInput = document.getElementById("donacion").value;
+  console.log(valorInput);
+
+  if (valorInput > 0) {
+    alert("Tu donacion fue exitosa!!");
+  } else {
+    alert("Tu donacion no puede ser menor a 1 peso");
+  }
+}
+
+function mostrarDonacion() {
+  var valorInput = document.getElementById("miDiv");
+  div.innerHTML = mostrarDonacion();
+}
